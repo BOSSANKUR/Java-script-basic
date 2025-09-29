bad0741bdd6ce39ab0c4aedcfcadb5789f311d55
Ankur Kumar <krankit285@gmail.com>
2025-09-29 22:47:20 +0530

    Operators In JS

---
+++ Operators.js
@@ -0,0 +1,29 @@
+let a=10;
+let b=5;
+
+//Arithematic Operators
+console.log(a+b);  //add
+console.log(a-b);  //subtract
+console.log(a*b);   //multiply
+console.log(a/b);   //divide
+console.log(a%b);     //ramainder
+//Square / Cube **.***
+console.log(a**2);  //Exponential Operator gives square
+console.log(a**3); //Cube
+
+//Relational Operator
+console.log(a==b);
+console.log(a<=b);
+console.log(a>=b);
+console.log(a!=b);
+
+
+//Converting String to Number
+let str="10";
+let num=Number(str);// prebuilt global constructor used to convert value into number
+console.log(typeof num);
+let nm=" Ankur";
+let ak=Number(nm);
+console.log(ak);//give  NaN=not a  Numebr
+console.log(typeof ak);
+
