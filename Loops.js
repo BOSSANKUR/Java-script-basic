a78db10cd879efb5043245082089b3e3f415cfe2
Ankur Kumar <krankit285@gmail.com>
2025-09-30 22:12:48 +0530

    Loops in JS

---
+++ Loops.js
@@ -0,0 +1,27 @@
+//Loops are same as c++ but → only some terms is changed
+
+//FOR loop------------------------
+let n = 10;
+for (let i = 1; i <= 10; i++) {
+  console.log(n, "X", i, "=", i * n);
+}
+console.log("\n");
+
+//While loop----------------------
+let nu = 5;
+let i = 1;
+while (i <= 10) {
+  console.log(nu, "X", i, "=", i * nu);
+  i++;
+}
+console.log();
+
+//Do while Loop---------------------
+let nm = 20;
+let j = 1;
+do {
+  console.log(nm,"X",j,"=",nm * j);
+  j++;
+} while (j <= 10);
+{
+}
